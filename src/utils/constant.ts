a92bfd3be16d89bfaf6ef2e257bf6bc1c26f4d0b
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2025-05-03 00:53:54 +0800

    Update src/utils/constant.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,5 @@
-export const API_BASE_Url = import.meta.env.VITE_API_BASE_URL;
+-export const API_BASE_Url = import.meta.env.VITE_API_BASE_URL;
++export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_URL = {
   AUTH_LOGIN: `/auth/login`,
