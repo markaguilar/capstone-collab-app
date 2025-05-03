781eccdece8e5b98643a45a39fe6f7e6e219d199
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2025-05-04 02:08:34 +0800

    I pushed a fix

--- src/features/global/globalSlice.ts
+++ src/features/global/globalSlice.ts
@@ -1,11 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "@/features/store.ts";
 
-const initialState = {};
+interface GlobalState {
+  isLoading: boolean;
+}
+
+const initialState: GlobalState = {
+  isLoading: true,
+};
 
 export const globalSlice = createSlice({
   name: "global",
   initialState,
   reducers: {},
 });
+
+export const selectIsGlobalLoading = (state: RootState) =>
+  state.global.isLoading;
 
 export default globalSlice.reducer;
